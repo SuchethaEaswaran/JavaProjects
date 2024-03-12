a21529fde65f01325e3a79d77e02980173a7ee55
Suchetha <sujithaeaswaran3@gmail.com>
2024-03-12 15:44:48 +0530

    Methods
    Methods

---
+++ Feb12_Methods/src/sampleA/Test.java
@@ -0,0 +1,15 @@
+package sampleA;
+
+public class Test {
+
+	public static void main(String[] args) {
+		methodTest();
+
+	}
+	private static void methodTest()
+	{
+		System.out.println("KavinSuchetha..");
+	}
+	
+
+}
