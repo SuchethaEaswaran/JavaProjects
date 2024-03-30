92a9a190e3a3a5a385b05c1fe209d0ae5e2c464b
Suchetha <sujithaeaswaran3@gmail.com>
2024-03-30 12:51:56 +0530

    While Loop
    While Loops

---
+++ Feb24_WhileLoop/src/test/SampleA.java
@@ -0,0 +1,16 @@
+package test;
+
+public class SampleA {
+
+	public static void main(String[] args) {
+		int a=1;
+		
+		while(a<=3)
+		{
+			System.out.println("SuchethaKavin");
+			a=a+1;
+		}
+
+	}
+
+}
---
+++ Feb24_WhileLoop/src/test/SampleB.java
@@ -0,0 +1,20 @@
+package test;
+
+public class SampleB {
+
+	public static void main(String[] args) {
+		int a=1;
+		
+		while(a<=10)
+		{
+			System.out.print(a);
+			System.out.print("x");
+			System.out.print("2");
+			System.out.print("=");
+			System.out.println(a*2);
+			a++;
+		}
+
+	}
+
+}
